f26677a8be8e6d0e92449fd58b5a145d2d9435de
randi sunardi <randisunardi1995@gmail.com>
2024-04-16 10:45:24 +0700

    update css

--- resources/views/layout/web/header.blade.php
+++ resources/views/layout/web/header.blade.php
@@ -1,38 +1,15 @@
 <!-- hero-area-start -->
 <div class="tp-hero-area tp-grey-insu p-relative z-index-1">
-    <img class="tp-hero-insu-shape p-absolute" src="assets/img/banner/insu/shape3.png" alt="shape">
     <div class="container">
        <div class="row">
           <div class="col-lg-6">
              <div class="tp-hero-wrapper-insu tp-hero-insu-spa">
                 <h2 class="tp-hero-title-insu animetion">Pusat Pembinaan <b> <span class="p-relative">Jabatan Fungsional Kepegawaian
-                    <img class="p-absolute tp-hero-insu-shape2" src="assets/img/banner/insu/shape4.png" alt="shape">
-                   </span> <br> Future</b> Life</h2>
-                <div class="tp-hero-useractive animetion">
-                   <div class="tp-btn-wrapper">
-                      <a class="tp-btn tp-btn-insu" href="about.html">EXPLORE MORE <i class="fa-solid fa-arrow-right"></i></a>
-                   </div>
-                   <div class="tp-hero-user">
-                      <div class="tp-hero-icon">
-                         <img src="assets/img/banner/dimond.png" alt="dimond">
-                      </div>
-                      <div class="tp-hero-active tp-hero-rating-insu">
-                         <b>1.80m</b>
-                         <span>Total Active User</span>
-                      </div>
-                   </div>
-                </div>
-                <div class="tp-hero-brands-insu animetion">
-                   <a href="#"> <img src="assets/img/banner/insu/brand3.png" alt="brand"></a>
-                   <a href="#"> <img src="assets/img/banner/insu/brand2.png" alt="brand"></a>
-                   <a href="#"> <img src="assets/img/banner/insu/brand1.png" alt="brand"></a>
-                </div>
              </div>
           </div>
           <div class="col-lg-6">
              <div class="tp-hero-insu-img-wrap h-100">
                 <div class="tp-hero-insu-img p-relative h-100">
-                   <img class="tp-hero-insu-img-1 animetion p-absolute" src="assets/img/banner/insu/main.png" alt="main">
                    <img class="tp-hero-insu-img-3 tptranslateX2 p-absolute" src="assets/img/banner/insu/shape2.png" alt="shape">
                    <img class="tp-hero-insu-img-4 up-down animetion p-absolute" src="assets/img/banner/insu/bird.png" alt="bird">
                    <img class="tp-hero-insu-img-6 up-down p-absolute" src="assets/img/banner/insu/balun2.png" alt="belun">
